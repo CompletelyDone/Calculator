e6c947344e4dad39394079d28b49e091175e37fd
Roman <roman.songoku.army@gmail.com>
2022-10-04 10:49:51 +0700

    04.10 UI

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -9,7 +9,7 @@ namespace ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        string display = "Doroy";
+        string display = "0";
         public string Display
         {
             get => display;
@@ -20,5 +20,22 @@ namespace ViewModels
             }
         }
         public string Dot => CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+        public Command <string> DigitPress { get; }
+        public MainViewModel()
+        {
+            DigitPress = new Command<string>(digitPress);
+        }
+
+        private void digitPress(string obj)
+        {
+            if (Display == "0" || Display == "-0")
+            {
+                Display = Display.Trim('0') + obj;
+            }
+            else
+            {
+                Display = Display + obj;
+            }
+        }
     }
 }
